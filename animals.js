1fe1979263a4af74f543b3417d871bbc6ce88d78
user <mutukukennedy34@gmail.com>
2023-06-25 13:49:40 +0300

    LatestCommit

--- animals.js
+++ animals.js
@@ -1,27 +1,80 @@
+animals()
+formlistener()
+function fetchanimallist(animal){
+  const animalitem = document.createElement("li")
+  
+  animalitem.innerHTML = `<p id = "myvt"> ${animal.name}</p>`
+  const mylist = document.getElementById("myanimallist")
+  mylist.append(animalitem)  
+}
 
 function createanimalelement(animal){
-const myunorderedanimalist = document. getElementById('unorderedlist')
+
  const listitem =document.createElement('li')
  listitem.id= 'animal'
  listitem.innerHTML = 
- `<img src ="${animal.image}">
- <div class = "content" >
+ `
  <h3> ${animal.name}</h3>
- <p> ${animal.id}</p>
-`
- myunorderedanimalist.append(listitem)
+ <img src ="${animal.image}">
+ <div class = "content" >
+ <p id = "myvt"> ${animal.votes}</p>
+ </div>
+ <div>
+ <button id = 'btn' > Vote </button>
+ <button id = 'reset' >   ResetVote </button>
+ </div>
+  `
 
-}
+  const myunorderedanimalist = document. getElementById('unorderedlist')
+  myunorderedanimalist.append(listitem)
+  
+     const button = document.getElementById('btn')
+     button.addEventListener('click' ,()=>{
+     const vote = document.getElementById('myvt')
+     vote.innerText =  (animal.votes) +1
+      })
+
+      const resetv = document.getElementById('reset')
+      resetv.addEventListener('click' ,()=>{
+        const vote = document.getElementById('myvt')
+        vote.innerText =  0
+    })
+  }
 //fetchdata   
 function animals(){
     fetch("http://localhost:3000/characters")
     .then(res => res.json())
-    .then(characters=>
-        characters.forEach(animal => createanimalelement(animal)))
-        
+    .then(jos=>
+     jos.forEach(animal => createanimalelement(animal)))
     }
+
+    function animalist(){
+        fetch("http://localhost:3000/characters")
+        .then(res => res.json())
+        .then(jos=>
+         jos.forEach(animal => createanimallist(animal)))
+        }
+
+     function formlistener(){
+     const myform = document.getElementsByTagName("form")
+      myform.addEventListener('submit' ,(event)=>{
+        event.preventDefault();
+        const animalnameinput = document.getElementById('Animalname')
+         fetch(`http://localhost:3000/characters/${animalnameinput.value}`)
+        .then(res => res.json())
+        .then(animaldata=>{
+      const addedid = document.getElementById('addedid')
+      const addedname = document.getElementById('addedname')
+      addedid.innerText = animaldata.id;
+      addedname.innerText = animaldata.name;
+      
+        })
+        
+  })
+      }
+
+        
+    // animalist()
+     
     
-function initalizeattempt(){
-    animals()
-}
-initalizeattempt()
+   
